4c8b73b9b5e66d06b4cf538940f3fd61c6cb6fe3
Phil Eaton <phil@eatonphil.com>
2021-06-11 22:11:28 +0000

    Block mobile browsers

--- ui/app.tsx
+++ ui/app.tsx
@@ -139,4 +139,10 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// SOURCE: https://stackoverflow.com/a/7995898/1507139
+const isMobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i);
+if (!isMobile) {
+  ReactDOM.render(<App />, document.getElementById('root'));
+} else {
+  document.getElementById('root').innerHTML = 'Please use a desktop web browser to view this app.';
+}
