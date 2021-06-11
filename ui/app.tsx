11809dd6a720e722491b0cfb49b206dc80a810ca
Phil Eaton <phil@eatonphil.com>
2021-06-11 22:12:37 +0000

    Fix for format

--- ui/app.tsx
+++ ui/app.tsx
@@ -140,9 +140,12 @@ function App() {
 }
 
 // SOURCE: https://stackoverflow.com/a/7995898/1507139
-const isMobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i);
+const isMobile = navigator.userAgent.match(
+  /(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i
+);
 if (!isMobile) {
   ReactDOM.render(<App />, document.getElementById('root'));
 } else {
-  document.getElementById('root').innerHTML = 'Please use a desktop web browser to view this app.';
+  document.getElementById('root').innerHTML =
+    'Please use a desktop web browser to view this app.';
 }
