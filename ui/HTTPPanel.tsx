817daa92afe4ecd0c9a464db9df534edd9bb9cac
Phil Eaton <phil@eatonphil.com>
2021-07-01 09:14:09 -0400

    Fix for copy-paste on mac, width of url attribute in http panel

--- ui/HTTPPanel.tsx
+++ ui/HTTPPanel.tsx
@@ -64,6 +64,8 @@ export function HTTPPanelDetails({
             panel.http.http.url = value;
             updatePanel(panel);
           }}
+          type="url"
+          autoWidth={true}
         />
       </div>
       <div className="form-row">
