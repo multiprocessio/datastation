e397e3f92a0025b02fd6284ad17a1af44cb7f02b
Phil Eaton <philneaton95@gmail.com>
2021-07-14 14:55:13 -0400

    Add back filepicker for desktop

--- ui/component-library/FileInput.tsx
+++ ui/component-library/FileInput.tsx
@@ -1,28 +1,71 @@
 import * as React from 'react';
 
-export function FileInput({
-  className,
-  onChange,
-  label,
-  disabled,
-  readOnly,
-  accept,
-}: {
+import { Input } from './Input';
+
+type Base = {
   className?: string;
-  onChange: (files: Array<File>) => void;
+  onChange: (fileName: string) => void;
   label?: string;
   disabled?: boolean;
   readOnly?: boolean;
   accept?: string;
-}) {
+  value: string;
+  onRead?: (value: ArrayBuffer) => void;
+  allowManualEntry?: boolean;
+  allowFilePicker?: boolean;
+};
+
+// SOURCE: https://stackoverflow.com/a/49725198/1507139
+type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
+  }[Keys];
+
+type Props = RequireAtLeastOne<Base, 'allowManualEntry' | 'allowFilePicker'>;
+
+export function FileInput({
+  className,
+  onChange,
+  onRead,
+  label,
+  disabled,
+  readOnly,
+  accept,
+  value,
+  allowManualEntry,
+}: Props) {
   let inputClass = `input ${className ? ' ' + className : ''}`;
+
+  const manualInput = (
+    <Input autoWidth={true} value={value} onChange={onChange} />
+  );
 
   const input = (
     <input
-      className={label ? '' : inputClass}
       type="file"
       onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-        onChange(Array.prototype.slice.apply(e.target.files));
+        const { files } = e.target;
+        if (!files.length) {
+          return;
+        }
+
+        const fr = new FileReader();
+
+        if (onRead) {
+          fr.onload = function () {
+            onRead(fr.result as ArrayBuffer);
+          };
+
+          fr.readAsArrayBuffer(files[0]);
+        }
+
+        // .path is available on Electron and gives the full path.
+        // Browsers otherwise only support giving you the file name
+        // without path for security reasons.
+        onChange(files[0].path || files[0].name);
       }}
       disabled={disabled}
       readOnly={readOnly}
@@ -30,14 +73,11 @@ export function FileInput({
     />
   );
 
-  if (label) {
-    return (
-      <label className={inputClass}>
-        {label}
-        {input}
-      </label>
-    );
-  }
-
-  return input;
+  return (
+    <label className={inputClass}>
+      {label}
+      {allowManualEntry && manualInput}
+      {input}
+    </label>
+  );
 }
