eba61d680207ffb4f1e7d95f51de6689d4a950a7
Phil Eaton <phil@eatonphil.com>
2021-07-14 20:16:23 +0000

    Add test for object too

--- ui/preview.test.js
+++ ui/preview.test.js
@@ -9,4 +9,7 @@ test('previewObject', () => {
 
   expect(previewObject([{ a: 1, b: 3, c: 5 }, { a: 12, b: 8, c: 5 }, { a: 13, b: 3, c: 9 }], 2)).toBe(
     '[\n  { "a": 1, ... },\n  { "a": 12, ... },\n  ...\n]');
+
+  expect(previewObject({ a: 12, b: [1, 3, 4], c: null, d: { n: "foo", m: 19, l: [12] } }, 4)).toBe(
+    '{\n  "a": 12,\n  "b": [ 1, 3, ... ],\n  "c": null,\n  "d": { "l": [ 12 ], "m": 19, ... }\n}');
 });
