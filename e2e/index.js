330f06fe8bb060a43f58c952539b7561f236562d
Phil Eaton <phil@eatonphil.com>
2021-07-18 19:29:27 +0000

    Fix for formatter

--- e2e/index.js
+++ e2e/index.js
@@ -8,24 +8,26 @@ async function run() {
   const app = new Application({
     path: path.join(process.cwd(), process.argv[2]),
   });
-  console.log('here2'); 
+  console.log('here2');
   await app.start();
-  console.log('here3'); 
+  console.log('here3');
   const isVisible = await app.browserWindow.isVisible();
-  console.log('here4'); 
+  console.log('here4');
   assert.equal(isVisible, true);
-  console.log('here5'); 
+  console.log('here5');
   const title = await app.client.getTitle();
-  console.log('here6'); 
+  console.log('here6');
   console.log(title);
   assert.equal(title, 'Blubberty');
-  console.log('here7'); 
+  console.log('here7');
   await app.stop();
 }
 
-run().then(function () {
-  process.exit(0);
-}).catch(function (error) {
-  console.error('Failed', error.message);
-  process.exit(1);
-});
+run()
+  .then(function () {
+    process.exit(0);
+  })
+  .catch(function (error) {
+    console.error('Failed', error.message);
+    process.exit(1);
+  });
