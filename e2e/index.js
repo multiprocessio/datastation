1f833eed52d6f7482121fc03762775e0659640c0
Phil Eaton <phil@eatonphil.com>
2021-07-18 17:47:21 +0000

    Fix e2e wrapper

--- e2e/index.js
+++ e2e/index.js
@@ -7,21 +7,28 @@ const app = new Application({
   path: path.join(process.cwd(), process.argv[2]),
 });
 
-app.start().then(function () {
-  // Check if the window is visible
-  return app.browserWindow.isVisible();
-}).then(function (isVisible) {
-  // Verify the window is visible
-  assert.equal(isVisible, true);
-}).then(function () {
-  // Get the window's title
-  return app.client.getTitle();
-}).then(function (title) {
-  // Verify the window's title
-  assert.equal(title, 'DataStation Community Edition');
-}).then(function () {
-  // Stop the application
-  return app.stop();
-}).catch(function (error) {
-  console.error('Test failed', error.message);
-});
+app
+  .start()
+  .then(function () {
+    // Check if the window is visible
+    return app.browserWindow.isVisible();
+  })
+  .then(function (isVisible) {
+    // Verify the window is visible
+    assert.equal(isVisible, true);
+  })
+  .then(function () {
+    // Get the window's title
+    return app.client.getTitle();
+  })
+  .then(function (title) {
+    // Verify the window's title
+    assert.equal(title, 'DataStation Community Edition');
+  })
+  .then(function () {
+    // Stop the application
+    return app.stop();
+  })
+  .catch(function (error) {
+    console.error('Test failed', error.message);
+  });
