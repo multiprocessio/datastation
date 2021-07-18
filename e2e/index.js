54efac8d8447874cc02aad2af1df6b83484d3e72
Phil Eaton <phil@eatonphil.com>
2021-07-18 19:18:02 +0000

    Rewrite as async/await

--- e2e/index.js
+++ e2e/index.js
@@ -3,32 +3,20 @@ const path = require('path');
 
 const Application = require('spectron').Application;
 
-const app = new Application({
-  path: path.join(process.cwd(), process.argv[2]),
-});
-
-app
-  .start()
-  .then(function () {
-    // Check if the window is visible
-    return app.browserWindow.isVisible();
-  })
-  .then(function (isVisible) {
-    // Verify the window is visible
-    assert.equal(isVisible, true);
-  })
-  .then(function () {
-    // Get the window's title
-    return app.client.getTitle();
-  })
-  .then(function (title) {
-    // Verify the window's title
-    assert.equal(title, 'DataStation Community Edition');
-  })
-  .then(function () {
-    // Stop the application
-    return app.stop();
-  })
-  .catch(function (error) {
-    console.error('Test failed', error.message);
+async function run() {
+  const app = new Application({
+    path: path.join(process.cwd(), process.argv[2]),
   });
+
+  await app.start();
+  const isVisible = await app.browserWindow.isVisible();
+  assert.equal(isVisible, true);
+  const title = await app.client.getTitle();
+  assert.equal(title, 'Blubberty');
+  await app.stop();
+}
+
+run().catch(function (error) {
+  console.error('Failed', error.message);
+  process.exit(1);
+});
