db18981357622056582ec4508a72b3f957446119
Phil Eaton <philneaton95@gmail.com>
2021-07-19 11:32:53 -0400

    Just dont wait for visible

--- e2e/index.js
+++ e2e/index.js
@@ -1,23 +1,26 @@
+const fs = require('fs');
 const assert = require('assert');
 const path = require('path');
 
 const Application = require('spectron').Application;
 
 const applicationPath = {
-  darwin: 'DataStation Community Edition.app/Contents/MacOS/DataStation Community Edition',
+  darwin:
+    'DataStation Community Edition.app/Contents/MacOS/DataStation Community Edition',
   win32: 'DataStation Community Edition.exe',
   linux: 'DataStation Community Edition',
 }[process.platform];
 
-const directory = path.join('releases', `DataStation Community Edition-${process.platform}-${process.arch}`);
+const directory = path.join(
+  'releases',
+  `DataStation Community Edition-${process.platform}-${process.arch}`
+);
 
 async function run() {
   const app = new Application({
     path: path.join(process.cwd(), directory, applicationPath),
   });
   await app.start();
-  const isVisible = await app.browserWindow.isVisible();
-  assert.equal(isVisible, true);
   const title = await app.client.getTitle();
   assert.equal(title, 'DataStation Community Edition');
   await app.stop();
