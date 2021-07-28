7d41f4fd22f2f85421bad7c89c75c13f335250f7
Phil Eaton <phil@eatonphil.com>
2021-07-28 00:42:41 +0000

    Fix external in watch and serve script

--- ui/scripts/watch_and_serve.sh
+++ ui/scripts/watch_and_serve.sh
@@ -6,7 +6,7 @@ set -eux
 yarn build-ui
 
 # Now watch for changes in the background and rebuild
-fswatch -x --event Created --event Removed --event Renamed --event Updated ./ui ./shared | grep --line-buffered -E "\\.(tsx|css|ts|js|jsx)" | xargs -n1 bash -c 'yarn esbuild ui/app.tsx --loader:.ts=tsx --loader:.js=jsx --bundle --sourcemap --outfile=build/ui.js && cp ui/style.css build/style.css' &
+fswatch -x --event Created --event Removed --event Renamed --event Updated ./ui ./shared | grep --line-buffered -E "\\.(tsx|css|ts|js|jsx)" | xargs -n1 bash -c 'yarn esbuild ui/app.tsx --loader:.ts=tsx --loader:.js=jsx "--external:fs/promises" --bundle --sourcemap --outfile=build/ui.js && cp ui/style.css build/style.css' &
 
 # Serve the pages
 python3 -m http.server --directory build 8080
