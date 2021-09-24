25a1db88656e1f47e91df4c70911cc48cf032997
Phil Eaton <phil@eatonphil.com>
2021-09-24 13:58:34 -0400

    Update deps, hacking doc

--- scripts/ci/prepare_windows.ps1
+++ scripts/ci/prepare_windows.ps1
@@ -1,4 +1,3 @@
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 Join-Path (Resolve-Path ~).Path "scoop\shims" >> $Env:GITHUB_PATH
 scoop install nodejs cmake python yarn zip jq curl
-yarn
