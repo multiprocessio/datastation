c81fb148fb8ccd40d180e894ac92305ba9a3ed71
Phil Eaton <phil@eatonphil.com>
2021-09-24 14:12:22 -0400

    Reset scripts

--- scripts/ci/prepare_linux.sh
+++ scripts/ci/prepare_linux.sh
@@ -3,3 +3,4 @@
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs cmake xvfb jq
 sudo npm install --global yarn
+yarn
--- scripts/ci/prepare_macos.sh
+++ scripts/ci/prepare_macos.sh
@@ -3,3 +3,4 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install nodejs cmake python3 jq
 npm install --global yarn
+yarn
