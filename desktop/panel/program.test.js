c3a618a4e6b83ba9985b0917810cb5516541b984
Phil Eaton <phil@eatonphil.com>
2021-10-03 19:36:27 -0400

    Try describing

--- desktop/panel/program.test.js
+++ desktop/panel/program.test.js
@@ -141,43 +141,45 @@ for (const t of LANGUAGES) {
     continue;
   }
 
-  // First pass runs in process, second pass runs in subprocess
-  for (const subprocessName of [
-    undefined,
-    path.join(CODE_ROOT, 'build', 'desktop_runner.js'),
-  ]) {
-    test(`runs ${t.type} programs via ${
-      subprocessName ? subprocessName : 'process'
-    }`, async () => {
-      const lp = new LiteralPanelInfo();
-      lp.literal.contentTypeInfo = { type: 'text/csv' };
-      lp.content = 'age,name\n12,Kev\n18,Nyra';
+  describe(t.type, () => {
+    // First pass runs in process, second pass runs in subprocess
+    for (const subprocessName of [
+      undefined,
+      path.join(CODE_ROOT, 'build', 'desktop_runner.js'),
+    ]) {
+      test(`runs ${t.type} programs via ${
+        subprocessName ? subprocessName : 'process'
+      }`, async () => {
+        const lp = new LiteralPanelInfo();
+        lp.literal.contentTypeInfo = { type: 'text/csv' };
+        lp.content = 'age,name\n12,Kev\n18,Nyra';
 
-      const pp = new ProgramPanelInfo();
-      pp.program.type = t.type;
-      pp.content = t.content;
+        const pp = new ProgramPanelInfo();
+        pp.program.type = t.type;
+        pp.content = t.content;
 
-      let finished = false;
-      const panels = [lp, pp];
-      await exports.withSavedPanels(
-        panels,
-        async (project) => {
-          const panelValueBuffer = fs.readFileSync(
-            getProjectResultsFile(project.projectName) + pp.id
-          );
-          expect(JSON.parse(panelValueBuffer.toString())).toStrictEqual([
-            { name: 'Kev', age: 22 },
-            { name: 'Nyra', age: 28 },
-          ]);
+        let finished = false;
+        const panels = [lp, pp];
+        await exports.withSavedPanels(
+          panels,
+          async (project) => {
+            const panelValueBuffer = fs.readFileSync(
+              getProjectResultsFile(project.projectName) + pp.id
+            );
+            expect(JSON.parse(panelValueBuffer.toString())).toStrictEqual([
+              { name: 'Kev', age: 22 },
+              { name: 'Nyra', age: 28 },
+            ]);
 
-          finished = true;
-        },
-        { evalPanels: true, subprocessName }
-      );
+            finished = true;
+          },
+          { evalPanels: true, subprocessName }
+        );
 
-      if (!finished) {
-        throw new Error('Callback did not finish');
-      }
-    }, 30_000);
-  }
+        if (!finished) {
+          throw new Error('Callback did not finish');
+        }
+      }, 30_000);
+    }
+  });
 }
