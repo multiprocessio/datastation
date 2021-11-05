0183852dc69ecaf3845b1ea5c227b950282591f1
Phil Eaton <phil@eatonphil.com>
2021-11-05 16:26:05 +0000

    Drop unlinkg

--- scripts/ci/prepare_macos.sh
+++ scripts/ci/prepare_macos.sh
@@ -4,7 +4,6 @@ set -eux
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install cmake jq node@16
-brew unlink node
 brew link --overwrite node@16
 npm install --global yarn
 yarn
