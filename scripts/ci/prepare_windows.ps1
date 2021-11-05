b581295aa02aff926250ed3b80bcd601ef9bc1ad
Phil Eaton <phil@eatonphil.com>
2021-11-05 00:06:06 +0000

    Pin to node16

--- scripts/ci/prepare_windows.ps1
+++ scripts/ci/prepare_windows.ps1
@@ -1,4 +1,4 @@
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 Join-Path (Resolve-Path ~).Path "scoop\shims" >> $Env:GITHUB_PATH
-scoop install nodejs cmake python yarn zip jq curl
+scoop install nodejs@16 cmake python yarn zip jq curl
 yarn
