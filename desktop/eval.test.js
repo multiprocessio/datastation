180abccfdb67624cf2878a002429300a0c8b1ed1
Phil Eaton <phil@eatonphil.com>
2021-11-05 11:25:58 -0400

    Add diff

--- desktop/eval.test.js
+++ desktop/eval.test.js
@@ -1,4 +1,23 @@
 const { makeEvalHandler } = require('./eval');
+const {
+  ProjectState,
+  ProjectPage,
+  ScheduledExport,
+  TablePanelInfo,
+  PanelResultMeta,
+} = require('@datastation/shared/state');
+
+const project = new ProjectState();
+project.pages = [new ProjectPage()];
+const lp = new LiteralPanelInfo({
+  contentTypeInfo:  { type: 'text/csv' },
+  content: 'age,name\n12,Kev\n18,Nyra',
+});
+
+const tp = new TablePanelInfo({ panelSource: lp.id })
+project.pages[0].panels = [lp, tp];
+
 test('', async () => {
-  // TODO: need to write tests for the runner
+  const { handler } = makeEvalHandler('build/desktop_runner.js');
+  // tODO: write to disk and eval
 });
