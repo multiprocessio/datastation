fe9f90bf16b48d22dac97e642939b3bbabf1b7be
Phil Eaton <phil@eatonphil.com>
2021-11-04 20:13:24 -0400

    Fix node install on mac

--- scripts/ci/prepare_macos.sh
+++ scripts/ci/prepare_macos.sh
@@ -3,6 +3,8 @@
 set -eux
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install cmake jq@16
+brew install cmake jq node@16
+brew unlink node
+brew link --overwrite node@16
 npm install --global yarn
 yarn
