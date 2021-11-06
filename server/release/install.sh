49bcff057cfe1cb7e1b6c52ddad15f0f3c7e327e
Phil Eaton <phil@eatonphil.com>
2021-11-06 23:52:56 +0000

    root dir not script dir

--- server/release/install.sh
+++ server/release/install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+ROOT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
 
 sudo mkdir -p /etc/datastation /usr/share/datastation/
-sudo cp -r "$SCRIPT_DIR/*" /usr/share/datastation
-sudo cp "$SCRIPT_DIR/config.json" /etc/datastation/
-sudo cp "$SCRIPT_DIR/datastation.service" /etc/systemd/system/
-sudo cp "$SCRIPT_DIR/datastation-exporter.timer" /etc/systemd/system/
+sudo cp -r "$ROOT_DIR/*" /usr/share/datastation/
+sudo cp "$ROOT_DIR/config.json" /etc/datastation/
+sudo cp "$ROOT_DIR/datastation.service" /etc/systemd/system/
+sudo cp "$ROOT_DIR/datastation-exporter.timer" /etc/systemd/system/
 
 sudo id -u datastation >/dev/null 2>&1 || sudo useradd -r -s /bin/false datastation
 sudo chown -R datastation:datastation /etc/datastation /usr/share/datastation
