72c94c43eb46328c42b51e832848636ffb291e5e
Phil Eaton <phil@eatonphil.com>
2021-11-06 23:59:40 +0000

    No quote

--- server/release/install.sh
+++ server/release/install.sh
@@ -5,10 +5,10 @@ set -ex
 ROOT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
 
 sudo mkdir -p /etc/datastation /usr/share/datastation/
-sudo cp -r "$ROOT_DIR/*" /usr/share/datastation/
-sudo cp "$ROOT_DIR/config.json" /etc/datastation/
-sudo cp "$ROOT_DIR/datastation.service" /etc/systemd/system/
-sudo cp "$ROOT_DIR/datastation-exporter.timer" /etc/systemd/system/
+sudo cp -r $ROOT_DIR/* /usr/share/datastation/
+sudo cp $ROOT_DIR/config.json /etc/datastation/
+sudo cp $ROOT_DIR/datastation.service /etc/systemd/system/
+sudo cp $ROOT_DIR/datastation-exporter.timer /etc/systemd/system/
 
 sudo id -u datastation >/dev/null 2>&1 || sudo useradd -r -s /bin/false datastation
 sudo chown -R datastation:datastation /etc/datastation /usr/share/datastation
