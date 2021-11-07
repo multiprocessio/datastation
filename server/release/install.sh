c71b40b70150fd34bb73061baf9aec9ba9693f3f
Phil Eaton <phil@eatonphil.com>
2021-11-07 00:04:11 +0000

    Correct location of config.json and systemd configs

--- server/release/install.sh
+++ server/release/install.sh
@@ -6,9 +6,9 @@ ROOT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
 
 sudo mkdir -p /etc/datastation /usr/share/datastation/
 sudo cp -r $ROOT_DIR/* /usr/share/datastation/
-sudo cp $ROOT_DIR/config.json /etc/datastation/
-sudo cp $ROOT_DIR/datastation.service /etc/systemd/system/
-sudo cp $ROOT_DIR/datastation-exporter.timer /etc/systemd/system/
+sudo cp $ROOT_DIR/release/config.json /etc/datastation/
+sudo cp $ROOT_DIR/release/datastation.service /etc/systemd/system/
+sudo cp $ROOT_DIR/release/datastation-exporter.timer /etc/systemd/system/
 
 sudo id -u datastation >/dev/null 2>&1 || sudo useradd -r -s /bin/false datastation
 sudo chown -R datastation:datastation /etc/datastation /usr/share/datastation
