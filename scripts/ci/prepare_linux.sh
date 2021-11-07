ba8aaa17b56717d929c2c6bce334664cc9c72c9e
Phil Eaton <phil@eatonphil.com>
2021-11-06 20:22:59 -0400

    Add build steps and install scripts for server (#84)
    * Sketching out new dockerfile
    
    * Start production server settings
    
    * Drop -u
    
    * Add server install tests
    
    * Correct runner
    
    * Server builds server_runner
    
    * More debugging
    
    * Create releases directory
    
    * x86
    
    * Correct naming
    
    * And another naming fix
    
    * Test install script
    
    * Test install script
    
    * Fix install
    
    * Debugging
    
    * Debugging
    
    * Debugging
    
    * root dir not script dir
    
    * No quote
    
    * Correct location of config.json and systemd configs
    
    * Enable service

--- scripts/ci/prepare_linux.sh
+++ scripts/ci/prepare_linux.sh
@@ -1,45 +1,43 @@
 #!/usr/bin/env bash
 
-set -eux
+set -ex
 
-# Set up Node.js, databases
+# Set up Node.js
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get update -y
-sudo apt-get install -y nodejs cmake xvfb jq postgresql postgresql-contrib
+sudo apt-get install -y nodejs cmake jq
 
-# Set up R
-#sudo apt-get install -y dirmngr gnupg apt-transport-https ca-certificates software-properties-common build-essential
-#wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-#sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-#sudo apt-get update -y
-#sudo apt install r-base
+if [[ "$1" == "--integration-tests" ]]; then
+    # Install xvfb for headless gui
+    sudo apt-get install -y xvfb
+    
+    # Allow R programs to install packages
+    sudo mkdir -p /usr/local/lib/R/site-library
+    sudo chown -R $USER /usr/local/lib/R/
 
-# Allow R programs to install packages
-sudo mkdir -p /usr/local/lib/R/site-library
-sudo chown -R $USER /usr/local/lib/R/
+    # # Set up MySQL
+    sudo service mysql start
+    sudo mysql -u root -proot --execute="CREATE USER 'test'@'localhost' IDENTIFIED BY 'test'";
+    sudo mysql -u root -proot --execute="CREATE DATABASE test";
+    sudo mysql -u root -proot --execute="GRANT ALL PRIVILEGES ON test.* TO 'test'@'localhost'";
 
-# # Set up MySQL
-sudo service mysql start
-sudo mysql -u root -proot --execute="CREATE USER 'test'@'localhost' IDENTIFIED BY 'test'";
-sudo mysql -u root -proot --execute="CREATE DATABASE test";
-sudo mysql -u root -proot --execute="GRANT ALL PRIVILEGES ON test.* TO 'test'@'localhost'";
-
-# # Set up PostgreSQL
-echo "
+    # # Set up PostgreSQL
+    sudo apt-get install postgresql postgresql-contrib
+    echo "
 local  test            test                md5
 host   test            test   localhost    md5
 local  all             all                 peer" | sudo tee /etc/postgresql/12/main/pg_hba.conf
-sudo service postgresql restart
-sudo -u postgres psql -c "CREATE USER test WITH PASSWORD 'test'"
-sudo -u postgres psql -c "CREATE DATABASE test"
-sudo -u postgres psql -c "GRANT ALL ON DATABASE test TO test"
+    sudo service postgresql restart
+    sudo -u postgres psql -c "CREATE USER test WITH PASSWORD 'test'"
+    sudo -u postgres psql -c "CREATE DATABASE test"
+    sudo -u postgres psql -c "GRANT ALL ON DATABASE test TO test"
 
-# # Set up ClickHouse
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4
-echo "deb https://repo.clickhouse.tech/deb/stable/ main/" | sudo tee /etc/apt/sources.list.d/clickhouse.list
-sudo apt-get update -y
-sudo apt-get install -y clickhouse-server clickhouse-client
-echo "
+    # # Set up ClickHouse
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E0C56BD4
+    echo "deb https://repo.clickhouse.tech/deb/stable/ main/" | sudo tee /etc/apt/sources.list.d/clickhouse.list
+    sudo apt-get update -y
+    sudo apt-get install -y clickhouse-server clickhouse-client
+    echo "
 <yandex>
   <users>
     <test>
@@ -54,9 +52,10 @@ echo "
     </test>
   </users>
 </yandex>" | sudo tee /etc/clickhouse-server/users.d/test.xml
-# See: https://community.atlassian.com/t5/Bitbucket-Pipelines-discussions/Broken-starting-clickhouse-in-a-docker-because-of-wrong/td-p/1689466
-setcap -r `which clickhouse` && echo "Cleaning caps success" || echo "Cleaning caps error"
-sudo service clickhouse-server start
+    # See: https://community.atlassian.com/t5/Bitbucket-Pipelines-discussions/Broken-starting-clickhouse-in-a-docker-because-of-wrong/td-p/1689466
+    setcap -r `which clickhouse` && echo "Cleaning caps success" || echo "Cleaning caps error"
+    sudo service clickhouse-server start
+fi
 
 # Set up project
 sudo npm install --global yarn
