3cde6cf91ecb414444780a9cbe7605df445ace25
Phil Eaton <phil@eatonphil.com>
2021-12-03 20:38:42 +0000

    Fixes for test

--- ui/panels/GraphPanel.tsx
+++ ui/panels/GraphPanel.tsx
@@ -143,27 +143,6 @@ function colorForDataset(
   return colorsSortedByRealLabelOrder;
 }
 
-// SOURCE: https://stackoverflow.com/a/13532993/1507139
-function shadeRGBPercent(color: string, percent: number) {
-  let R = parseInt(color.substring(1, 3), 16);
-  let G = parseInt(color.substring(3, 5), 16);
-  let B = parseInt(color.substring(5, 7), 16);
-
-  R = Math.floor((R * (100 + percent)) / 100);
-  G = Math.floor((G * (100 + percent)) / 100);
-  B = Math.floor((B * (100 + percent)) / 100);
-
-  R = R < 255 ? R : 255;
-  G = G < 255 ? G : 255;
-  B = B < 255 ? B : 255;
-
-  const RR = R.toString(16).length == 1 ? '0' + R.toString(16) : R.toString(16);
-  const GG = G.toString(16).length == 1 ? '0' + G.toString(16) : G.toString(16);
-  const BB = B.toString(16).length == 1 ? '0' + B.toString(16) : B.toString(16);
-
-  return '#' + RR + GG + BB;
-}
-
 export function GraphPanel({ panel, panels }: PanelBodyProps<GraphPanelInfo>) {
   const {
     state: { theme },
