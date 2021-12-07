f0e323c8c9c1a8e8d457d40e314f7527d3c0a7dc
Phil Eaton <phil@eatonphil.com>
2021-12-07 20:13:40 +0000

    Overwrite brew go link

--- scripts/ci/prepare_macos.sh
+++ scripts/ci/prepare_macos.sh
@@ -4,5 +4,6 @@ set -eux
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install cmake jq go
+brew link --overwrite go
 npm install --global yarn
 yarn
