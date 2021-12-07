b4176ab4f63c6231168f996ac26deedb600599c1
Phil Eaton <phil@eatonphil.com>
2021-12-07 20:13:15 +0000

    Symlink to usrloclbin

--- scripts/ci/prepare_linux.sh
+++ scripts/ci/prepare_linux.sh
@@ -11,8 +11,9 @@ sudo apt-get install -y nodejs cmake jq
 sudo curl -LO https://go.dev/dl/go1.17.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.17.4.linux-amd64.tar.gz
-sudo ln -s /usr/local/go/bin/go go
-sudo ln -s /usr/local/go/bin/gofmt gofmt
+sudo ln -s /usr/local/go/bin/go /usr/local/bin/go
+sudo ln -s /usr/local/go/bin/gofmt /usr/local/bin/gofmt
+
 
 if [[ "$1" == "--integration-tests" ]]; then
     # Install xvfb for headless gui
