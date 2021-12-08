7de7d605d136e55b9c7bc85954ea6d2c5893e82d
Phil Eaton <phil@eatonphil.com>
2021-12-07 19:25:24 -0500

    Fix for format

--- desktop/panel/databases/clickhouse.test.js
+++ desktop/panel/databases/clickhouse.test.js
@@ -11,42 +11,45 @@ for (const subprocess of [
   { node: path.join(CODE_ROOT, 'build', 'desktop_runner.js') },
   { go: path.join(CODE_ROOT, 'build', 'go_desktop_runner') },
 ]) {
-  test('runs clickhouse query via ' + (subprocess.node || subprocess.go), async () => {
-    if (process.platform !== 'linux') {
-      return;
+  test(
+    'runs clickhouse query via ' + (subprocess.node || subprocess.go),
+    async () => {
+      if (process.platform !== 'linux') {
+        return;
+      }
+
+      const connectors = [
+        new DatabaseConnectorInfo({
+          type: 'clickhouse',
+          address: 'localhost',
+          username: 'test',
+          password_encrypt: new Encrypt('test'),
+        }),
+      ];
+      const dp = new DatabasePanelInfo();
+      dp.database.connectorId = connectors[0].id;
+      dp.content = 'SELECT 42 AS number';
+
+      let finished = false;
+      const panels = [dp];
+      await withSavedPanels(
+        panels,
+        async (project) => {
+          const panelValueBuffer = fs.readFileSync(
+            getProjectResultsFile(project.projectName) + dp.id
+          );
+          expect(JSON.parse(panelValueBuffer.toString())).toStrictEqual([
+            { number: 42 },
+          ]);
+
+          finished = true;
+        },
+        { evalPanels: true, connectors, subprocessName: subprocess }
+      );
+
+      if (!finished) {
+        throw new Error('Callback did not finish');
+      }
     }
-
-    const connectors = [
-      new DatabaseConnectorInfo({
-	type: 'clickhouse',
-	address: 'localhost',
-	username: 'test',
-	password_encrypt: new Encrypt('test'),
-      }),
-    ];
-    const dp = new DatabasePanelInfo();
-    dp.database.connectorId = connectors[0].id;
-    dp.content = 'SELECT 42 AS number';
-
-    let finished = false;
-    const panels = [dp];
-    await withSavedPanels(
-      panels,
-      async (project) => {
-	const panelValueBuffer = fs.readFileSync(
-          getProjectResultsFile(project.projectName) + dp.id
-	);
-	expect(JSON.parse(panelValueBuffer.toString())).toStrictEqual([
-          { number: 42 },
-	]);
-
-	finished = true;
-      },
-      { evalPanels: true, connectors, subprocessName: subprocess }
-    );
-
-    if (!finished) {
-      throw new Error('Callback did not finish');
-    }
-  });
+  );
 }
