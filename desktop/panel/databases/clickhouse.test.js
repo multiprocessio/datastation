726e804439e75e6af256fb5e0e41636aed8f3f5d
Phil Eaton <phil@eatonphil.com>
2021-12-07 19:24:32 -0500

    Run clickhouse tests through go runner too

--- desktop/panel/databases/clickhouse.test.js
+++ desktop/panel/databases/clickhouse.test.js
@@ -7,41 +7,46 @@ const {
 } = require('../../../shared/state');
 const { withSavedPanels } = require('../testutil');
 
-test(`runs clickhouse query`, async () => {
-  if (process.platform !== 'linux') {
-    return;
-  }
+for (const subprocess of [
+  { node: path.join(CODE_ROOT, 'build', 'desktop_runner.js') },
+  { go: path.join(CODE_ROOT, 'build', 'go_desktop_runner') },
+]) {
+  test('runs clickhouse query via ' + (subprocess.node || subprocess.go), async () => {
+    if (process.platform !== 'linux') {
+      return;
+    }
 
-  const connectors = [
-    new DatabaseConnectorInfo({
-      type: 'clickhouse',
-      address: 'localhost',
-      username: 'test',
-      password_encrypt: new Encrypt('test'),
-    }),
-  ];
-  const dp = new DatabasePanelInfo();
-  dp.database.connectorId = connectors[0].id;
-  dp.content = 'SELECT 42 AS number';
+    const connectors = [
+      new DatabaseConnectorInfo({
+	type: 'clickhouse',
+	address: 'localhost',
+	username: 'test',
+	password_encrypt: new Encrypt('test'),
+      }),
+    ];
+    const dp = new DatabasePanelInfo();
+    dp.database.connectorId = connectors[0].id;
+    dp.content = 'SELECT 42 AS number';
 
-  let finished = false;
-  const panels = [dp];
-  await withSavedPanels(
-    panels,
-    async (project) => {
-      const panelValueBuffer = fs.readFileSync(
-        getProjectResultsFile(project.projectName) + dp.id
-      );
-      expect(JSON.parse(panelValueBuffer.toString())).toStrictEqual([
-        { number: 42 },
-      ]);
+    let finished = false;
+    const panels = [dp];
+    await withSavedPanels(
+      panels,
+      async (project) => {
+	const panelValueBuffer = fs.readFileSync(
+          getProjectResultsFile(project.projectName) + dp.id
+	);
+	expect(JSON.parse(panelValueBuffer.toString())).toStrictEqual([
+          { number: 42 },
+	]);
 
-      finished = true;
-    },
-    { evalPanels: true, connectors }
-  );
+	finished = true;
+      },
+      { evalPanels: true, connectors, subprocessName: subprocess }
+    );
 
-  if (!finished) {
-    throw new Error('Callback did not finish');
-  }
-});
+    if (!finished) {
+      throw new Error('Callback did not finish');
+    }
+  });
+}
