6a5c6eae7439e3f3adc973f3c20a3407955d57c6
Phil Eaton <phil@eatonphil.com>
2021-12-07 19:20:54 -0500

    Add simpler tests for mysql/postgres and go runner

--- desktop/panel/database.test.js
+++ desktop/panel/database.test.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const { CODE_ROOT } = require('../constants');
 const { getProjectResultsFile } = require('../store');
 const fs = require('fs');
 const {
@@ -6,10 +8,17 @@ const {
   DatabasePanelInfo,
   DatabaseConnectorInfo,
 } = require('../../shared/state');
-const { makeEvalHandler } = require('./eval');
 const { withSavedPanels } = require('./testutil');
 
 const DATABASES = [
+  {
+    type: 'postgres',
+    query: 'SELECT 1',
+  },
+  {
+    type: 'mysql',
+    query: 'SELECT 1',
+  },
   {
     type: 'postgres',
     query: 'SELECT name, age::INT - 10 AS age FROM DM_getPanel(0)',
@@ -21,51 +30,69 @@ const DATABASES = [
   // TODO: add sqlite tests
 ];
 
-for (const t of DATABASES) {
-  describe(t.type, () => {
-    test(`runs ${t.type} query`, async () => {
-      if (process.platform !== 'linux') {
-        return;
-      }
+for (const subprocess of [
+  { node: path.join(CODE_ROOT, 'build', 'desktop_runner.js') },
+  { go: path.join(CODE_ROOT, 'build', 'go_desktop_runner') },
+]) {
+  for (const t of DATABASES) {
+    // Go runner doesn't support DM_getPanel yet
+    if (t.query !== 'SELECT 1' && subprocess.go) {
+      continue;
+    }
 
-      const lp = new LiteralPanelInfo();
-      lp.literal.contentTypeInfo = { type: 'text/csv' };
-      lp.content = 'age,name\n19,Kate\n20,Bake';
+    describe(
+      t.type + 'running via ' + (subprocess.node || subprocess.go),
+      () => {
+        test(`runs ${t.type} query`, async () => {
+          if (process.platform !== 'linux') {
+            return;
+          }
 
-      const connectors = [
-        new DatabaseConnectorInfo({
-          type: t.type,
-          database: 'test',
-          address: 'localhost',
-          username: 'test',
-          password_encrypt: new Encrypt('test'),
-        }),
-      ];
-      const dp = new DatabasePanelInfo();
-      dp.database.connectorId = connectors[0].id;
-      dp.content = t.query;
+          const lp = new LiteralPanelInfo();
+          lp.literal.contentTypeInfo = { type: 'text/csv' };
+          lp.content = 'age,name\n19,Kate\n20,Bake';
 
-      let finished = false;
-      const panels = [lp, dp];
-      await withSavedPanels(
-        panels,
-        async (project) => {
-          const panelValueBuffer = fs.readFileSync(
-            getProjectResultsFile(project.projectName) + dp.id
+          const connectors = [
+            new DatabaseConnectorInfo({
+              type: t.type,
+              database: 'test',
+              address: 'localhost',
+              username: 'test',
+              password_encrypt: new Encrypt('test'),
+            }),
+          ];
+          const dp = new DatabasePanelInfo();
+          dp.database.connectorId = connectors[0].id;
+          dp.content = t.query;
+
+          let finished = false;
+          const panels = [lp, dp];
+          await withSavedPanels(
+            panels,
+            async (project) => {
+              const panelValueBuffer = fs.readFileSync(
+                getProjectResultsFile(project.projectName) + dp.id
+              );
+
+              expect(JSON.parse(panelValueBuffer.toString())).toStrictEqual(
+                t.query === 'SELECT 1'
+                  ? 1
+                  : [
+                      { name: 'Kate', age: 9 },
+                      { name: 'Bake', age: 10 },
+                    ]
+              );
+
+              finished = true;
+            },
+            { evalPanels: true, connectors, subprocessName: subprocess }
           );
-          expect(JSON.parse(panelValueBuffer.toString())).toStrictEqual([
-            { name: 'Kate', age: 9 },
-            { name: 'Bake', age: 10 },
-          ]);
 
-          finished = true;
-        },
-        { evalPanels: true, connectors }
-      );
-
-      if (!finished) {
-        throw new Error('Callback did not finish');
+          if (!finished) {
+            throw new Error('Callback did not finish');
+          }
+        });
       }
-    });
-  });
+    );
+  }
 }
