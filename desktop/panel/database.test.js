f75dc842d21273dfdf695a7f57bae05650bdd3f6
Phil Eaton <phil@eatonphil.com>
2021-12-09 18:39:49 +0000

    Try 0.0.0.0

--- desktop/panel/database.test.js
+++ desktop/panel/database.test.js
@@ -51,7 +51,7 @@ const vendorOverride = {
     address: 'localhost?sslmode=disable',
   },
   sqlserver: {
-    address: 'tcp://127.0.0.1',
+    address: 'tcp://0.0.0.0',
     username: 'sa',
     password: '1StrongPwd!!',
     database: 'test',
