8859f7e30e7ab3fc2ef4af18c75e3d1bb58b7c0d
Phil Eaton <phil@eatonphil.com>
2021-12-15 01:35:03 +0000

    Start on log parsing tests

--- desktop/panel/file.test.js
+++ desktop/panel/file.test.js
@@ -4,8 +4,7 @@ const { CODE_ROOT } = require('../constants');
 const path = require('path');
 const fs = require('fs');
 const { getProjectResultsFile } = require('../store');
-const { HTTPPanelInfo, HTTPConnectorInfo } = require('../../shared/state');
-const { FilePanelInfo } = require('../../shared/state');
+const { FilePanelInfo, ContentTypeInfo } = require('../../shared/state');
 const {
   withSavedPanels,
   translateBaselineForType,
@@ -18,6 +17,55 @@ const testPath = path.join(CODE_ROOT, 'testdata');
 const baseline = JSON.parse(
   fs.readFileSync(path.join(testPath, 'userdata.json').toString())
 );
+
+const REGEXP_TESTS = [
+  {
+    filename: 'nginx.access.log',
+    contentTypeInfo: new ContentTypeInfo(''),
+    expected: [
+      { remote: '', host: '', user: '', time: '', method: '', path: '', code: '', size: '', referer: '', agent: '', http_x_forwarded_for: '' },
+    ],
+  },
+  {
+    filename: 'syslogrfc3164.log',
+    contentTypeInfo: new ContentTypeInfo('text/syslogrfc3164'),
+    expected: [
+      { pri: '34', time: 'Oct 11 22:14:15', host: 'mymachine', ident: '', pid: '', message: 'failed for lonvick on /dev/pts/9' },
+      { pri: '0', time: '1990 Oct 22 10:52:01 TZ-6', host: 'scapegoat.dmz.example.org 10.1.2.3', ident: 'sched', pid: '0', message: 'That\'s All Folks!'}
+    ],
+  },
+  {
+    filename: 'syslogrfc5424.log',
+    contentTypeInfo: new ContentTypeInfo('text/syslogrfc5424'),
+    expected: [
+      { pri: '34', time: '2003-10-11T22:14:15.003Z', host: 'mymachine.example.com', ident: 'su', pid: '', msgid: 'ID47', extradata: '', message: 'BOM\'su root\' failed for lonvick on /dev/pts/8' },
+      { pri: '165', time: '2003-08-24T05:14:15.003Z', host: 'mymachine.example.com', ident: 'su', pid: '', msgid: 'ID47', extradata: '', message: 'BOM\'su root\' failed for lonvick on /dev/pts/8' },
+    ],
+  },
+  {
+    filename: 'apache.access.log',
+    contentTypeInfo: new ContentTypeInfo('text/apache2access'),
+    expected: [
+      { host: '', user: '', time: '', method: '', path: '', code: '', size: '', referer: '', agent: '' }
+    ],
+  },
+  {
+    filename: 'apache.error.log',
+    contentTypeInfo: new ContentTypeInfo('text/apache2error'),
+    expected: [
+      { time: '', level: '', pid: '', client: '', message: '' },
+    ],
+  },
+  {
+    filename: 'custom.log',
+    contentTypeInfo: new ContentTypeInfo('text/regexplines', '(?<method>[a-zA-Z]+) (?<path>[a-zA-Z/?=]+) (?<domain>[a-zA-Z/?=]+) (?<address>[0-9.]+)'),
+    expected: [
+      { method: 'GET', path: '/path2', domain: 'www.google.com', address: '8.8.8.10' },
+      { method: 'POST', path: '/mypath', domain: 'old.misc.org', address: '99.10.10.1' },
+      { method: 'DELETE', path: '/path?x=y', domain: 'nine.org', address: '10.0.0.1' },
+    ],
+  },
+];
 
 for (const subprocessName of [
   undefined,
@@ -63,6 +111,42 @@ for (const subprocessName of [
               expect(replaceBigInt(value)).toStrictEqual(
                 replaceBigInt(typeBaseline)
               );
+            },
+            { evalPanels: true, subprocessName }
+          );
+        }, 10_000);
+      }
+    );
+  }
+
+  for (const t of REGEXP_TESTS) {
+    const fp = new FilePanelInfo({
+      name: path.join(testPath, t.filename),
+      contentTypeInfo: t.contentTypeInfo,
+    });
+
+    const panels = [fp];
+
+    describe(
+      'eval ' +
+        t.filename
+        ' file via ' +
+        (subprocessName ? subprocessName.go || subprocessName.node : 'memory'),
+      () => {
+        test('correct result', () => {
+          return withSavedPanels(
+            panels,
+            (project) => {
+              // Grab result
+              const value = JSON.parse(
+                fs
+                  .readFileSync(
+                    getProjectResultsFile(project.projectName) + fp.id
+                  )
+                  .toString()
+              );
+
+	      expect(value).toStrictEqual(t.expected);
             },
             { evalPanels: true, subprocessName }
           );
