821449363beba768e3ffecab66c823ade5da3942
Phil Eaton <phil@eatonphil.com>
2021-12-22 04:35:12 +0000

    Add more comments

--- runner/cmd/dsq/scripts/test.sh
+++ runner/cmd/dsq/scripts/test.sh
@@ -5,6 +5,7 @@ set -e
 types="csv parquet json"
 
 for t in $types; do
+    echo "Testing $t (pipe)."
     sqlcount="$(cat ./testdata/userdata.$t | ./dsq $t 'SELECT COUNT(1) AS c FROM {}' | jq '.[0].c')"
     if [[ "$sqlcount" != "1000" ]]; then
 	echo "Bad SQL count for $t (pipe). Expected 1000, got $sqlcount."
@@ -13,6 +14,7 @@ for t in $types; do
 	echo "Pipe $t test successful."
     fi
 
+    echo "Testing $t (file)."
     sqlcount="$(./dsq ./testdata/userdata.$t 'SELECT COUNT(1) AS c FROM {}' | jq '.[0].c')"
     if [[ "$sqlcount" != "1000" ]]; then
 	echo "Bad SQL count for $t (file). Expected 1000, got $sqlcount."
