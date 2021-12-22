88c905cc0d9df18712180726071a1b7b880a6b7f
Phil Eaton <phil@eatonphil.com>
2021-12-22 04:27:05 +0000

    Uninstall 1.15

--- scripts/ci/prepare_macos.sh
+++ scripts/ci/prepare_macos.sh
@@ -3,7 +3,7 @@
 set -eux
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew uninstall go@1.17
+brew uninstall go@1.15
 brew install cmake jq go r julia
 npm install --global yarn
 yarn
