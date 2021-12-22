0ace3e824e6ab8386e13d5d22b86a30e76d0a1b2
Phil Eaton <phil@eatonphil.com>
2021-12-22 03:43:03 +0000

    Add test script

---
+++ runner/cmd/dsq/scripts/test.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+types="csv parquet json"
+
+for t in $types; do
+    sqlcount="$(cat ./testdata/userdata.$t | ./dsq $t 'SELECT COUNT(1) AS c FROM {}' | jq '.[0].c')"
+    if [[ "$sqlcount" != "1000" ]]; then
+	echo "Bad SQL count for $t (pipe). Expected 1000, got $sqlcount."
+	exit 1
+    else
+	echo "Pipe $t test successful."
+    fi
+
+    sqlcount="$(./dsq ./testdata/userdata.$t 'SELECT COUNT(1) AS c FROM {}' | jq '.[0].c')"
+    if [[ "$sqlcount" != "1000" ]]; then
+	echo "Bad SQL count for $t (file). Expected 1000, got $sqlcount."
+	exit 1
+    else
+	echo "File $t test successful."
+    fi
+done
