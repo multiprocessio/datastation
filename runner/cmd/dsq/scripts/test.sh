9b78b7e36d8970157438308d8772b83fe4b3e169
Phil Eaton <phil@eatonphil.com>
2021-12-22 04:57:03 +0000

    More debugging

--- runner/cmd/dsq/scripts/test.sh
+++ runner/cmd/dsq/scripts/test.sh
@@ -6,6 +6,7 @@ types="csv parquet json"
 
 for t in $types; do
     echo "Testing $t (file)."
+    ls -lah ./testdata/userdata.$t
     sqlcount="$(./dsq -v ./testdata/userdata.$t 'SELECT COUNT(1) AS c FROM {}' | jq '.[0].c')"
     if [[ "$sqlcount" != "1000" ]]; then
 	echo "Bad SQL count for $t (file). Expected 1000, got $sqlcount."
