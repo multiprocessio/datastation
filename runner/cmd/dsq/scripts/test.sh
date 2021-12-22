6e6736a5c29db37ea0e263c35286b4611ea267ad
Phil Eaton <phil@eatonphil.com>
2021-12-22 04:48:52 +0000

    Rearrange tests

--- runner/cmd/dsq/scripts/test.sh
+++ runner/cmd/dsq/scripts/test.sh
@@ -5,15 +5,6 @@ set -ex
 types="csv parquet json"
 
 for t in $types; do
-    echo "Testing $t (pipe)."
-    sqlcount="$(cat ./testdata/userdata.$t | ./dsq $t 'SELECT COUNT(1) AS c FROM {}' | jq '.[0].c')"
-    if [[ "$sqlcount" != "1000" ]]; then
-	echo "Bad SQL count for $t (pipe). Expected 1000, got $sqlcount."
-	exit 1
-    else
-	echo "Pipe $t test successful."
-    fi
-
     echo "Testing $t (file)."
     sqlcount="$(./dsq ./testdata/userdata.$t 'SELECT COUNT(1) AS c FROM {}' | jq '.[0].c')"
     if [[ "$sqlcount" != "1000" ]]; then
@@ -21,5 +12,14 @@ for t in $types; do
 	exit 1
     else
 	echo "File $t test successful."
+    fi
+
+    echo "Testing $t (pipe)."
+    sqlcount="$(cat ./testdata/userdata.$t | ./dsq $t 'SELECT COUNT(1) AS c FROM {}' | jq '.[0].c')"
+    if [[ "$sqlcount" != "1000" ]]; then
+	echo "Bad SQL count for $t (pipe). Expected 1000, got $sqlcount."
+	exit 1
+    else
+	echo "Pipe $t test successful."
     fi
 done
