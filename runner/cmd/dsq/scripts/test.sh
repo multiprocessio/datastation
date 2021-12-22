45ca0bb9ce4dc01ec488c09eaf5e0d030c284674
Phil Eaton <phil@eatonphil.com>
2021-12-22 04:28:08 +0000

    Drop cat

--- runner/cmd/dsq/scripts/test.sh
+++ runner/cmd/dsq/scripts/test.sh
@@ -13,8 +13,6 @@ for t in $types; do
 	echo "Pipe $t test successful."
     fi
 
-    cat ./testdata/userdata.$t
-
     sqlcount="$(./dsq ./testdata/userdata.$t 'SELECT COUNT(1) AS c FROM {}' | jq '.[0].c')"
     if [[ "$sqlcount" != "1000" ]]; then
 	echo "Bad SQL count for $t (file). Expected 1000, got $sqlcount."
