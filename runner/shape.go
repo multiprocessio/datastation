bb75221f520ef671abb90ea5c0a2fb6ec27c746c
Phil Eaton <phil@eatonphil.com>
2022-01-10 23:32:33 -0500

    Add support for reading partial shapes from disk (#139)

--- runner/shape.go
+++ runner/shape.go
@@ -1,6 +1,9 @@
 package runner
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"os"
+)
 
 type ShapeKind string
 
@@ -138,4 +141,124 @@ func GetArrayShape(id string, raw []map[string]interface{}, sampleSize int) (*Sh
 			},
 		},
 	}, nil
+}
+
+// TODO: support things that aren't arrays
+func GetShape(id string, value interface{}, sampleSize int) (*Shape, error) {
+	switch t := value.(type) {
+	case []interface{}:
+		var objects []map[string]interface{}
+		for _, row := range t {
+			switch tt := row.(type) {
+			case map[string]interface{}:
+				objects = append(objects, tt)
+			default:
+				return nil, makeErrNotAnArrayOfObjects(id)
+			}
+		}
+
+		return GetArrayShape(id, objects, sampleSize)
+	case []map[string]interface{}:
+		return GetArrayShape(id, t, sampleSize)
+	default:
+		return nil, makeErrNotAnArrayOfObjects(id)
+	}
+}
+
+const DefaultShapeMaxBytesToRead = 100_000
+
+func ShapeFromFile(file, id string, maxBytesToRead int, sampleSize int) (*Shape, error) {
+	fd, err := os.Open(file)
+	if err != nil {
+		return nil, edse(err)
+	}
+
+	stat, err := fd.Stat()
+	if err != nil {
+		return nil, edse(err)
+	}
+
+	size := stat.Size()
+
+	if size < int64(maxBytesToRead) {
+		var value interface{}
+		decoder := json.NewDecoder(fd)
+		err := decoder.Decode(&value)
+		if err != nil {
+			return nil, edse(err)
+		}
+
+		return GetShape(id, value, sampleSize)
+	}
+
+	done := false
+	var f []byte
+	var incomplete []byte
+	inString := false
+
+	for !done {
+		buf := make([]byte, 1024)
+		bytesRead, err := fd.Read(buf)
+		if err != nil {
+			return nil, edse(err)
+		}
+
+		read := buf[:bytesRead]
+	outer:
+		for i, c := range read {
+			if c != '"' && inString {
+				continue
+			}
+
+			switch c {
+			case '"':
+				var previous byte = ' '
+				if i > 0 {
+					previous = read[i-1]
+				} else if len(f) > 0 {
+					previous = f[len(f)-1]
+				}
+
+				isEscaped := previous == '\\'
+				if !isEscaped {
+					inString = !inString
+				}
+			case '{', '[':
+				incomplete = append(incomplete, c)
+			case ']', '}':
+				if len(f)+len(read) >= maxBytesToRead {
+					// Need to not count additional openings after this
+					done = true
+					read = read[:i]
+					break outer
+				}
+
+				// Otherwise, pop it
+				incomplete = incomplete[:len(incomplete)-1]
+				break
+			}
+		}
+
+		f = append(f, read...)
+		if bytesRead < cap(buf) {
+			break
+		}
+	}
+
+	for i := len(incomplete) - 1; i >= 0; i-- {
+		last := incomplete[i]
+		if last == '{' {
+			f = append(f, '}')
+		} else {
+			f = append(f, ']')
+		}
+	}
+
+	var value interface{}
+	err = json.Unmarshal(f, &value)
+	if err != nil {
+		return nil, edse(err)
+	}
+
+	return GetShape(id, value, sampleSize)
 }
