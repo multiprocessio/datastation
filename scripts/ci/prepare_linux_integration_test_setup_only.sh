abf9df2af329f2ad47c15835b2695e5922955bd5
Phil Eaton <phil@eatonphil.com>
2022-01-25 23:02:47 -0500

    Add scylla

--- scripts/ci/prepare_linux_integration_test_setup_only.sh
+++ scripts/ci/prepare_linux_integration_test_setup_only.sh
@@ -106,6 +106,6 @@ docker run -d -p 8086:8086 -e "DOCKER_INFLUXDB_INIT_MODE=setup" -e "DOCKER_INFLU
 docker run -d -p 8086:8087 -e "INFLUXDB_HTTP_AUTH_ENABLED=true" -e "INFLUXDB_ADMIN_USER=test" -e "INFLUXDB_ADMIN_PASSWORD=test" influxdb:1.7
 
 # Start up scylla
-docker run -d scylladb/scylla --smp 1
+docker run -d scylladb/scylla --smp 1 --authenticator PasswordAuthenticator
 
 # TODO: might be worth switching to docker-compose at some point...
