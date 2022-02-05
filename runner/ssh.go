8176a49ce8e7af40ecab6a80ba6cf8625b3bb7d0
Phil Eaton <phil@eatonphil.com>
2022-02-05 14:52:01 -0500

    Pipe instead

--- runner/ssh.go
+++ runner/ssh.go
@@ -203,6 +203,54 @@ fi`, remoteFileName, remoteFileName)
 	return nil
 }
 
+// SOURCE: https://www.stavros.io/posts/proxying-two-connections-go/
+func chanFromConn(conn net.Conn) chan []byte {
+	c := make(chan []byte)
+
+	go func() {
+		b := make([]byte, 1024)
+
+		for {
+			n, err := conn.Read(b)
+			if n > 0 {
+				res := make([]byte, n)
+				// Copy the buffer so it doesn't get changed while read by the recipient.
+				copy(res, b[:n])
+				c <- res
+			}
+			if err != nil {
+				c <- nil
+				break
+			}
+		}
+	}()
+
+	return c
+}
+
+// SOURCE: https://www.stavros.io/posts/proxying-two-connections-go/
+func pipe(conn1 net.Conn, conn2 net.Conn) {
+	chan1 := chanFromConn(conn1)
+	chan2 := chanFromConn(conn2)
+
+	for {
+		select {
+		case b1 := <-chan1:
+			if b1 == nil {
+				return
+			} else {
+				conn2.Write(b1)
+			}
+		case b2 := <-chan2:
+			if b2 == nil {
+				return
+			} else {
+				conn1.Write(b2)
+			}
+		}
+	}
+}
+
 func withRemoteConnection(si *ServerInfo, host, port string, cb func(host, port string) error) error {
 	if si == nil {
 		return cb(host, port)
@@ -236,19 +284,7 @@ func withRemoteConnection(si *ServerInfo, host, port string, cb func(host, port 
 			return
 		}
 
-		go func() {
-			_, err = io.Copy(remoteConn, localConn)
-			if errC != nil {
-				errC <- err
-			}
-
-		}()
-
-		// Remote server
-		go func() {
-			_, err = io.Copy(localConn, remoteConn)
-			errC <- err
-		}()
+		pipe(localConn, remoteConn)
 	}()
 
 	localPort := localConn.Addr().(*net.TCPAddr).Port
