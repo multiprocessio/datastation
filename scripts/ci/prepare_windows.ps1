eb01938cf82dcc45dc91a9ca1ad175f420d43ece
Phil Eaton <phil@eatonphil.com>
2022-02-23 20:15:23 -0500

    Unclear why canvas is deleted

--- scripts/ci/prepare_windows.ps1
+++ scripts/ci/prepare_windows.ps1
@@ -2,7 +2,6 @@ Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.
 Join-Path (Resolve-Path ~).Path "scoop\shims" >> $Env:GITHUB_PATH
 scoop install nodejs@16 cmake python yarn zip jq curl go r julia
 yarn
-Remove-Item -Recurse -Force "node_modules/canvas"
 go install github.com/google/go-jsonnet/cmd/jsonnet@latest
 
 New-Alias zip 7z
