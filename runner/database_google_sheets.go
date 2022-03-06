9bf7e7f22de950dc3fb089d46188b5eb7078e86c
Phil Eaton <phil@eatonphil.com>
2022-03-06 21:13:34 +0000

    Fixes for Google sheets

--- runner/database_google_sheets.go
+++ runner/database_google_sheets.go
@@ -2,14 +2,48 @@ package runner
 
 import (
 	"context"
+	"fmt"
 	"io"
-	_ "strings"
+	"strings"
 
-	_ "github.com/multiprocessio/go-json"
+	"github.com/multiprocessio/go-json"
 
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
 )
+
+func writeGoogleSheet(sheet *sheets.Sheet, w *jsonutil.StreamEncoder, row *map[string]interface{}) error {
+	var header []string
+	if len(sheet.Data) != 1 {
+		return edsef("Too few or too many data grids (%d), should not be possible.", len(sheet.Data))
+	}
+
+	for _, r := range sheet.Data[0].RowData {
+		// Is first row, fill out header.
+		// TODO: let user opt out of headers being required
+		if header == nil {
+			for _, cell := range r.Values {
+				// TODO: if not a real header this might be nil?
+				header = append(header, *cell.UserEnteredValue.StringValue)
+			}
+			continue
+		}
+
+		// Otherwise is data row.
+		for i, header := range header {
+			if i < len(r.Values)-1 {
+				(*row)[header] = r.Values[i].UserEnteredValue.StringValue
+			} else {
+				// Need to reset the cell
+				(*row)[header] = nil
+			}
+		}
+
+		// TODO: could be cells without a header
+	}
+
+	return nil
+}
 
 func evalGoogleSheets(panel *PanelInfo, dbInfo DatabaseConnectorInfoDatabase, w io.Writer) error {
 	ctx := context.Background()
@@ -24,81 +58,56 @@ func evalGoogleSheets(panel *PanelInfo, dbInfo DatabaseConnectorInfoDatabase, w 
 		return edsef("Unable to retrieve Sheets client: %v", err)
 	}
 
-	_, err = srv.Spreadsheets.Get(panel.Database.Table).Do()
+	rsp, err := srv.Spreadsheets.Get(panel.Database.Table).Do()
 	if err != nil {
 		return edsef("Unable to retrieve data from sheet: %v", err)
 	}
 
-	return nil
+	sheets := rsp.Sheets
 
-	// TODO
+	sharedRow := map[string]interface{}{}
+	// Single sheet files get flattened into just an array, not a dict mapping sheet name to sheet contents
+	if len(sheets) == 1 {
+		return withJSONArrayOutWriterFile(w, func(w *jsonutil.StreamEncoder) error {
+			return writeGoogleSheet(sheets[0], w, &sharedRow)
+		})
+	}
 
-	// sheets := rsp.Sheets
+	sheetNames := map[string]string{}
+	return withJSONOutWriter(w, "{", "}", func() error {
+		for i, sheet := range sheets {
+			if i == 0 {
+				_, err := w.Write([]byte(",\n"))
+				if err != nil {
+					return err
+				}
+			}
 
-	// // Single sheet files get flattened into just an array, not a dict mapping sheet name to sheet contents
-	// if len(sheets) == 1 {
-	// 	var header []string
-	// 	row := map[string]interface{}{}
-	// 	return withJSONArrayOutWriterFile(w, func(w *jsonutil.StreamEncoder) error {
-	// 		if len(sheets[0].Data) != 1 {
-	// 			return edsef("Too few or too many data grids (%d), should not be possible.", len(sheets[0].Data))
-	// 		}
+			name := sheet.Properties.Title
+			nth := 0
+			for {
+				_, exists := sheetNames[name]
+				if !exists {
+					break
+				}
 
-	// 		for i, r := range sheets[0].Data[0].RowData {
-	// 			// Is first row, fill out header.
-	// 			// TODO: let user opt out of headers being required
-	// 			if header == nil {
-	// 				for i, cell := range r.Values {
-	// 					// TODO: if not a real header this might be nil?
-	// 					header = append(header, *cell.UserEnteredValue.StringValue)
-	// 				}
-	// 				continue
-	// 			}
+				nth++
+				name = fmt.Sprintf("%s%d", sheet.Properties.Title, nth)
+			}
+			sheetNameKey := `"` + strings.ReplaceAll(name, `"`, `\\"`) + `":`
+			_, err := w.Write([]byte(sheetNameKey))
+			if err != nil {
+				return err
+			}
 
-	// 			// Otherwise is data row.
-	// 			for i, header := range header {
-	// 				if i < len(r.Values) -1 {
-	// 					row[header] = r.Values[i].UserEnteredValue.StringValue
-	// 				} else {
-	// 					// Need to reset the cell
-	// 					row[header] = nil
-	// 				}
-	// 			}
+			err = withJSONArrayOutWriter(w, func(w *jsonutil.StreamEncoder) error {
+				return writeGoogleSheet(sheet, w, &sharedRow)
+			})
+			if err != nil {
+				return err
+			}
+		}
 
-	// 			// TODO: could be cells without a header
-	// 		}
-
-	// 		return nil
-	// 	})
-	// }
-
-	// return withJSONOutWriter(w, "{", "}", func() error {
-	// 	for i, sheet := range sheets {
-	// 		if i == 0 {
-	// 			_, err := w.Write([]byte(",\n"))
-	// 			if err != nil {
-	// 				return err
-	// 			}
-	// 		}
-
-	// 		sheetNameKey := `"` + strings.ReplaceAll(sheet, `"`, `\\"`) + `":`
-	// 		_, err := w.Write([]byte(sheetNameKey))
-	// 		if err != nil {
-	// 			return err
-	// 		}
-
-	// 		err = withJSONArrayOutWriter(w, func(w *jsonutil.StreamEncoder) error {
-	// 			rows, err := in.GetRows(sheet)
-	// 			if err != nil {
-	// 				return err
-	// 			}
-	// 			return writeSheet(rows, w)
-	// 		})
-	// 		if err != nil {
-	// 			return err
-	// 		}
-	// 	}
-
-	// 	return nil
-	// })
+		return nil
+	})
 }
