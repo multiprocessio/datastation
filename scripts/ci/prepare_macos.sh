ffe9ee4901410b7fa8b785f9b5748b0b029f7516
Phil Eaton <phil@eatonphil.com>
2022-03-06 21:42:29 +0000

    Overwrite

--- scripts/ci/prepare_macos.sh
+++ scripts/ci/prepare_macos.sh
@@ -5,7 +5,7 @@ set -eux
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew uninstall go@1.15
 brew install cmake jq go r julia node@16 npm
-brew link node@16
+brew link --overwrite node@16
 go install github.com/google/go-jsonnet/cmd/jsonnet@latest
 npm install --global yarn
 yarn
