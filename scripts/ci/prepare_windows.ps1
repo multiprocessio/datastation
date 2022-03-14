bd518cd6f0ab9f52c0d47fa55be929356d7db753
Phil Eaton <phil@eatonphil.com>
2022-03-14 17:24:53 -0400

    Add go bin to path

--- scripts/ci/prepare_windows.ps1
+++ scripts/ci/prepare_windows.ps1
@@ -5,5 +5,6 @@ scoop install nodejs@16 cmake python yarn zip jq curl go r julia
 yarn
 go install github.com/google/go-jsonnet/cmd/jsonnet@latest
 go install github.com/multiprocessio/httpmirror@latest
+Join-Path (Resolve-Path ~).Path "go\bin" >> $Env:GITHUB_PATH
 
 New-Alias zip 7z
