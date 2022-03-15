864fab8999e10ab6928d34994a6bb90d7ae78834
Phil Eaton <phil@eatonphil.com>
2022-03-15 20:52:42 +0000

    Move to Go 1.18

--- scripts/ci/prepare_macos.sh
+++ scripts/ci/prepare_macos.sh
@@ -4,7 +4,7 @@ set -eux
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew uninstall go@1.15
-brew install cmake jq go r julia node@16 npm
+brew install cmake jq go@1.18 r julia node@16 npm
 brew link --overwrite node@16
 go install github.com/google/go-jsonnet/cmd/jsonnet@latest
 go install github.com/multiprocessio/httpmirror@latest
