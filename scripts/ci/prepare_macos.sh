e49008f37553fff321b4f90dee423b583f7a69fe
Phil Eaton <phil@eatonphil.com>
2022-03-15 21:09:05 +0000

    Try not using brew

--- scripts/ci/prepare_macos.sh
+++ scripts/ci/prepare_macos.sh
@@ -2,9 +2,15 @@
 
 set -eux
 
+# Install go
+sudo curl -LO https://go.dev/dl/go1.18.darwin-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.18.darwin-amd64.tar.gz
+sudo mv /usr/local/go/bin/go /usr/local/bin/go
+sudo mv /usr/local/go/bin/gofmt /usr/local/bin/gofmt
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew uninstall go@1.15
-brew install cmake jq go@1.18 r julia node@16 npm
+brew install cmake jq r julia node@16 npm
 brew link --overwrite node@16
 go install github.com/google/go-jsonnet/cmd/jsonnet@latest
 go install github.com/multiprocessio/httpmirror@latest
