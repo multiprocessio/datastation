622b4481bf12a30ca248c4bd3b5d5894b3212e40
Phil Eaton <phil@eatonphil.com>
2022-04-02 03:25:26 +0000

    Fix graph/table shortcut button spacing

--- ui/PanelList.tsx
+++ ui/PanelList.tsx
@@ -160,14 +160,14 @@ export function PanelList({
         />
 
         {offerToGraph ? (
-          <span title="Generate a graph for the above panel">
+          <span className="new-panel-short" title="Generate a graph for the above panel">
             <Button onClick={() => makePanel('graph', 'Graph of')} icon>
               <IconChartBar />
             </Button>
           </span>
         ) : null}
         {offerToTable ? (
-          <span title="Generate a table for the above panel">
+          <span className="new-panel-short" title="Generate a table for the above panel">
             <Button onClick={() => makePanel('table', 'Data from')} icon>
               <IconTable />
             </Button>
