8f049e98b4a210fff672fcf63fbb822a9a9efd4f
Phil Eaton <phil@eatonphil.com>
2022-05-05 11:37:10 -0400

    More build script cleanup

--- server/release/install.sh
+++ server/release/install.sh
@@ -8,7 +8,7 @@ sudo mkdir -p /etc/datastation /usr/share/datastation/
 sudo cp -r $ROOT_DIR/* /usr/share/datastation/
 sudo cp $ROOT_DIR/release/config.yaml /etc/datastation/
 sudo cp $ROOT_DIR/release/datastation.service /etc/systemd/system/
-sudo cp $ROOT_DIR/release/datastation-exporter.timer /etc/systemd/system/
+# sudo cp $ROOT_DIR/release/datastation-exporter.timer /etc/systemd/system/
 
 sudo id -u datastation >/dev/null 2>&1 || sudo useradd -r -s /bin/false datastation
 sudo chown -R datastation:datastation /etc/datastation /usr/share/datastation
