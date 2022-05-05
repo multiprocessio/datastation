b82b2883e5016557da11a712f3930cc1c4cda93d
Phil Eaton <phil@eatonphil.com>
2022-05-05 16:21:53 +0000

    More renaming

--- server/release/install.sh
+++ server/release/install.sh
@@ -4,8 +4,8 @@ set -ex
 
 ROOT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/.."
 
-sudo mkdir -p /etc/datastation /usr/share/datastation/
-sudo cp -r $ROOT_DIR/* /usr/share/datastation/
+sudo mkdir -p /etc/datastation /usr/share
+sudo mv $ROOT_DIR /usr/share/datastation
 sudo cp $ROOT_DIR/release/config.yaml /etc/datastation/
 sudo cp $ROOT_DIR/release/datastation.service /etc/systemd/system/
 # sudo cp $ROOT_DIR/release/datastation-exporter.timer /etc/systemd/system/
