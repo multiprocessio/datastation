47d3bc9f556477c097417e94a049b7a8ac33043b
Phil Eaton <phil@eatonphil.com>
2022-05-06 15:52:05 +0000

    Add systemd testing dockerfile

--- server/scripts/test_release_zip.sh
+++ server/scripts/test_release_zip.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-cid=$(docker run -d -p 8080:8834 fedora tail -f /dev/null)
+docker build . -f ./server/scripts/test_systemd_dockerfile -t fedora-systemd
+cid=$(docker run -d -p 8834:8080 fedora-systemd:latest)
 docker cp ./releases/$1 $cid:/
 
 function c_run() {
     docker exec $cid bash -c "$1"
 }
 
-c_run "dnf install -y zip && dnf module reset nodejs && dnf module install -y nodejs:16"
 c_run "unzip /$1"
 c_run "/datastation/release/install.sh"
-c_run "truncate /etc/datastation/config.yaml"
+c_run "truncate --size 0 /etc/datastation/config.yaml"
 c_run "service datastation restart"
 
 curl localhost:8834
