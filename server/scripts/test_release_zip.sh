bb58a55d66a1653c7109df2b6f0703c6f31bdd9d
Phil Eaton <phil@eatonphil.com>
2022-05-06 15:32:20 +0000

    Closer on release test script

--- server/scripts/test_release_zip.sh
+++ server/scripts/test_release_zip.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cid=$(docker run -d fedora tail -f /dev/null)
+cid=$(docker run -d -p 8080:8834 fedora tail -f /dev/null)
 docker cp ./releases/$1 $cid:/
 
 function c_run() {
@@ -12,3 +12,7 @@ function c_run() {
 c_run "dnf install -y zip && dnf module reset nodejs && dnf module install -y nodejs:16"
 c_run "unzip /$1"
 c_run "/datastation/release/install.sh"
+c_run "truncate /etc/datastation/config.yaml"
+c_run "service datastation restart"
+
+curl localhost:8834
