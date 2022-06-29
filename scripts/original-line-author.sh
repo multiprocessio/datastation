6165f9d14a678b0b483450cc3c4daa03dfadcfee
Phil Eaton <phil@eatonphil.com>
2022-06-29 13:13:42 +0000

    Add usage script to original line author script

--- scripts/original-line-author.sh
+++ scripts/original-line-author.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# This script helps you find the original author of a line that hasn't
+# changed but has moved around.
+#
+# Usage:
+#   ./scripts/original-line-author.sh runner/file.go "err := r.SkipRows(offset)"
+
 for commit in $(git --no-pager log --reverse --pretty=format:"%h"); do
     if [[ -z $(git ls-tree -r "$commit" --name-only | grep "$1") ]]; then
 	continue
