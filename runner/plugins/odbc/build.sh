30f3a5f90c918eceed3f1e53230fdc59b40f236b
tridao <daominhtri0503@gmail.com>
2022-06-29 20:23:48 -0400

    uncomment

--- runner/plugins/odbc/build.sh
+++ runner/plugins/odbc/build.sh
@@ -2,14 +2,14 @@
 
 set -ex
 
-# # Download  unixODBC source code and compile
-# curl -LO http://www.unixodbc.org/unixODBC-2.3.11.tar.gz
-# tar -xvf unixODBC-2.3.11.tar.gz
-# cd unixODBC-2.3.11
-# ./configure
-# make
-# sudo make install
-# cd .. && sudo rm -rf unixODBC-2.3.11
+# Download  unixODBC source code and compile
+curl -LO http://www.unixodbc.org/unixODBC-2.3.11.tar.gz
+tar -xvf unixODBC-2.3.11.tar.gz
+cd unixODBC-2.3.11
+./configure
+make
+sudo make install
+cd .. && sudo rm -rf unixODBC-2.3.11
 
 race=""
 if [[ "$1" == "-race" ]]; then
