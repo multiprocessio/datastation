bbae06d33ddf0f99ef495adf87ccff4c0567f65c
Ben Schuddeboom <benzel@live.nl>
2022-07-09 21:08:14 +0200

    use newer mongosh package

--- scripts/ci/prepare_linux_integration_test_setup_only.sh
+++ scripts/ci/prepare_linux_integration_test_setup_only.sh
@@ -128,8 +128,8 @@ neo4j="$(docker run -d -p7474:7474 -p7687:7687 --env NEO4J_AUTH=neo4j/password n
 
 # Start up mongodb and install mongosh (shell)
 docker run -d -e "MONGO_INITDB_ROOT_USERNAME=test" -e "MONGO_INITDB_DATABASE=test" -e "MONGO_INITDB_ROOT_PASSWORD=test" -p 27017:27017 mongo:5
-curl -LO https://github.com/mongodb-js/mongosh/releases/download/v1.1.9/mongodb-mongosh_1.1.9_amd64.deb
-sudo apt-get install -y ./mongodb-mongosh_1.1.9_amd64.deb
+curl -LO https://github.com/mongodb-js/mongosh/releases/download/v1.1.9/mongodb-mongosh_1.5.0_amd64.deb
+sudo apt-get install -y ./mongodb-mongosh_1.5.0_amd64.deb
 
 ## LOAD DATA ##
 
