9d28a7565ba56c603c7ce5e19e70812fc0fd6481
Phil Eaton <phil@eatonphil.com>
2022-07-23 14:56:10 +0000

    Adjust coverage for credential database test

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,23 @@ module.exports = {
   // Only on linux do all tests run
   coverageThreshold:
     process.platform === 'linux'
-      ? {
-          global: {
-            statements: 54,
-            branches: 43,
-            functions: 37,
-            lines: 55,
-          },
-        }
+      ? process.argv.includes('desktop/panel/credential_database.test.js')
+        ? {
+            global: {
+              statements: 15,
+              branches: 13,
+              lines: 15,
+              functions: 9,
+            },
+          }
+        : {
+            global: {
+              statements: 54,
+              branches: 43,
+              functions: 37,
+              lines: 55,
+            },
+          }
       : undefined,
   preset: 'ts-jest/presets/js-with-ts',
   transformIgnorePatterns: [
