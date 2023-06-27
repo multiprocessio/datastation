f4b8905f04fb0bf133998a6c537d49e0e3923b51
Phil Eaton <phil@eatonphil.com>
2023-06-26 21:26:23 -0400

    Drop ee

--- ee/desktop/preload.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2022 Multiprocess Labs LLC
-
-import { bridgeAsyncRPC } from '../../desktop/bridge';
-
-bridgeAsyncRPC();
--- ee/type-overrides/ace.d.ts
+++
@@ -1,4 +0,0 @@
-// Copyright 2022 Multiprocess Labs LLC
-
-declare module 'ace-builds/src-min-noconflict/ace';
-declare module 'ace-builds/src-min-noconflict/ext-language_tools';
